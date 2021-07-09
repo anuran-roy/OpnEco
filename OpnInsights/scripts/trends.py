f2bcc882164c3773f4c2e653e8f41395ff25c3b1
anuran <anuranroy02@gmail.com>
2021-07-09 14:29:11 +0530

    Removed the 4 terms limit for OpnInsights

--- OpnInsights/scripts/trends.py
+++ OpnInsights/scripts/trends.py
@@ -4,27 +4,45 @@ import pandas as pd
 # import seaborn as sns
 import plotly
 # import kaleido
-print("Setup complete")
+import time
+# print("Setup complete")
 
 class Trends:
 
     def getTrend(self, text):
         text = text.replace(r"\r\n", "\n")
         terms = text.split("\n")
-        trend = tr(hl="en-US", tz=30)
-        trend.build_payload(kw_list=terms)
+        i = 0
+        br = []
+        while(i<len(terms)):
+            br.append(terms[i:i+4])
+            i+=4
+        try:
+            df = pd.DataFrame()
+            dft = pd.DataFrame()
+            for i in range(len(br)):
+                trend = tr(hl="en-US", tz=365)
+                trend.build_payload(kw_list=br[i])
+                dft = trend.interest_over_time()
+                dft = dft.drop("isPartial", axis=1)
+            #     print("DFT=\n",dft)
+                if i == 0:
+                    df = dft.copy()
+                else:
+                    df = pd.concat([df, dft], axis = 1)
+            #     print("DF=\n",df)
+                time.sleep(2)
+                
+            col = list(df.columns)
 
-        df = trend.interest_over_time()
-        df["date"] = df.index
+            pt = px.line(df, x=df.index, y = col)# ["Data Science", "Web Development", "Ethical Hacking", "Cybersecurity"])
+            # pt.show()
+            fig = plotly.offline.plot(pt, include_plotlyjs=True, output_type='div')
 
-        col = list(df.columns)
-        col.remove("date")
-        col.remove("isPartial")
-        col
-        pt = px.line(df, x="date", y = col)
-        fig = plotly.offline.plot(pt, include_plotlyjs=True, output_type='div')
+            return [fig, df]
 
-        return [fig, df]
+        except Exception as e:
+            return [f"<h1>An error was encountered. Error log: {e}</h1>", None]
 
 def execute(text):
     ob = Trends()
